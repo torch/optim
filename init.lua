13889aefcd8e8d37b56ee98778c5bcb1519a7f2c
Clement Farabet <cfarabet@twitter.com>
2014-10-13 19:55:22 -0400

    Added nag to init.

--- init.lua
+++ init.lua
@@ -7,6 +7,7 @@ optim = {}
 torch.include('optim', 'sgd.lua')
 torch.include('optim', 'cg.lua')
 torch.include('optim', 'asgd.lua')
+torch.include('optim', 'nag.lua')
 torch.include('optim', 'fista.lua')
 torch.include('optim', 'lbfgs.lua')
 torch.include('optim', 'adagrad.lua')
@@ -18,6 +19,6 @@ torch.include('optim', 'lswolfe.lua')
 -- helpers
 torch.include('optim', 'polyinterp.lua')
 
--- tools 
+-- tools
 torch.include('optim', 'ConfusionMatrix.lua')
 torch.include('optim', 'Logger.lua')
