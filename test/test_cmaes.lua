b3cf7c22441bae6959732acff18f09775226a1fc
MaxReimann <max.reimann@student.hpi.uni-potsdam.de>
2015-12-21 00:56:10 +0100

    Adapt cmaes to API, enable n-dimensional tensors as input

--- test/test_cmaes.lua
+++ test/test_cmaes.lua
@@ -1,10 +1,19 @@
 require 'torch'
 require 'optim'
 
-
 require 'rosenbrock'
 require 'l2'
 
-x = torch.Tensor(4):fill(0)
-config = {maxEval=10000, sigma=0.5}
-x,fx,i=optim.cmaes(rosenbrock,x, config)
+x = torch.Tensor(2):fill(0)
+config = {maxEval=10000, sigma=0.5, ftarget=0.00001, verb_disp=0}
+x,fx,i=optim.cmaes(rosenbrock,x,config)
+
+
+print('Rosenbrock test')
+print()
+print('Number of function evals = ',i)
+print('x=');print(x)
+print('fx=')
+for i=1,#fx do print(i,fx[i]); end
+print()
+print()
